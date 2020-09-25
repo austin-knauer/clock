3faf221d099483fb7c7cdd97cfcef5411d249fac
Austin <austinknauer@alumni.colostate.edu>
2020-09-24 23:50:40 -0600

    Updates

---
+++ precache-manifest.98a768b8920cf275766005466e9cdeab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04dde9df6e9c64faecc7ee2e200d239a",
+    "url": "/clock/index.html"
+  },
+  {
+    "revision": "20caaf35bab95e1078a8",
+    "url": "/clock/static/css/main.9a120d58.chunk.css"
+  },
+  {
+    "revision": "cf7250cbc21950730489",
+    "url": "/clock/static/js/2.730278a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clock/static/js/2.730278a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20caaf35bab95e1078a8",
+    "url": "/clock/static/js/main.976fc56e.chunk.js"
+  },
+  {
+    "revision": "9fbb0c64d7d7588fb297",
+    "url": "/clock/static/js/runtime-main.bf40dc10.js"
+  },
+  {
+    "revision": "b36f058a622e5041ef3e59882c712649",
+    "url": "/clock/static/media/beep.b36f058a.mp3"
+  }
+]);
